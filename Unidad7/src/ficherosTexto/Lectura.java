4ea891f0722774e23ed8bc3a2aa638875c26ea76
Martín Portela <martinportelasegui@gmail.com>
2025-05-28 17:05:02 +0200

    FicherosUnidad

--- Unidad7/src/ficherosTexto/Lectura.java
+++ Unidad7/src/ficherosTexto/Lectura.java
@@ -4,6 +4,7 @@ package ficherosTexto;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
 
 public class Lectura 
